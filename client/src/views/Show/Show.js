0167e975ce21aa1c3e0ac1868a6062bf3ba38652
Suraj Shende <surajshende247@gmail.com>
2022-12-17 18:04:13 +0530

    feat: api integration for show and add

--- client/src/views/Show/Show.js
+++ client/src/views/Show/Show.js
@@ -1,42 +1,36 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
 import "./Show.css"
 
 import FoodItemCard from '../../component/FoodItemCard/FoodItemCard'
 
 function Show() {
+  const [foodItems, setFoodItems] = useState([])
 
-  const [foodItems, setFoodItems] = useState([
-    {
-      "id": 1,
-      "title": "Idli",
-      "price": 15,
-      "category": "breakfast"
-    },
-    {
-      "id": 2,
-      "title": "Dosa",
-      "price": 20,
-      "category": "breakfast"
-    },
-    {
-      "id": 3,
-      "title": "Biryani",
-      "price": 100,
-      "category": "lunch"
-    }
-  ])
+  async function loadData(){
+    // load data from API
+    const response = await axios.get('/all-food-items')
+    setFoodItems(response.data.data)
+  }
+
+  useEffect(()=>{
+    loadData()
+  }, [])
 
   return (
     <div>
       <h1>Show All Food Items</h1>
       {
-        foodItems.map((item)=>{
+        foodItems.map((item, index)=>{
             return (
             <FoodItemCard
               id={item.id}
               title={item.title}
               category={item.category}
-              price={item.price} />)
+              price={item.price}
+              key={index}
+              />)
         })
       }
     </div>
